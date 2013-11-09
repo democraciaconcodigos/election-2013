7c3ebb8f66acb288e3ad9cb70d8f00d134529853
Gonzalo Almeida <flecox@gmail.com>
2013-11-09 14:19:56 -0300

    fix name bug

--- static/script.js
+++ static/script.js
@@ -7,6 +7,13 @@ function setColors(listNames) {
     return result;
 }
 
+function getName(listName, name) {
+    if (name in listName) {
+        return listName[name];
+    }
+    return name;
+}
+
 function getLeader(data) {
     var votes = data['votos'];
     var leader = "";
@@ -33,88 +40,89 @@ function getLeader(data) {
 }
 
 var addCommas = function (nStr) {
-        nStr += '';
-        x = nStr.split('.');
-        x1 = x[0];
-        x2 = x.length > 1 ? '.' + x[1] : '';
-        var rgx = /(\d+)(\d{3})/;
-        while (rgx.test(x1)) {
-            x1 = x1.replace(rgx, '$1' + ',' + '$2');
-        }
-        return x1 + x2;
+    nStr += '';
+    x = nStr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
 };
 
-    function titleCase(str) {
-        if (!str) {
-            return "";
+function titleCase(str) {
+    if (!str) {
+        return "";
+    }
+    result = str.toLowerCase();
+    return result.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function getPercentage(value, total) {
+    return ((value/total)*100).toFixed(1);
+}
+
+var $width = $(window).width();
+var center = [initialLat, initialLon];
+var zoom = 8;
+
+if ($width <= 767) {
+    center = [initialLat, initialLon];
+    zoom = 7;
+}
+
+if ($width <= 479) {
+    zoom = 7;
+}
+
+var map = L.map('map', {
+    zoomControl: false,
+    minZoom: 8
+}).setView(center, zoom);
+// Base tiles
+var mapquestLayer = new L.TileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>,<a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.',
+    subdomains: ['otile1','otile2','otile3','otile4']
+});
+map.addLayer(mapquestLayer);
+
+// Styling tricks
+var radiusConstant = 0.5;
+var getRadius = function(value) {
+    return Math.sqrt(value)*radiusConstant;
+};
+
+colorList = setColors(listNames);
+// Fire off to get our GeoJSON
+$.getJSON(geoJsonUrl, function(response) {
+
+    //get parties and assign colors
+
+    // Pull in the circle layer from GeoJSON
+    var circleLayer = L.geoJson(response, {
+        pointToLayer: function (feature, latlng) {
+            var leaderData = getLeader(feature['properties']);
+            var marker = new L.circleMarker(latlng, {
+                fillColor: colorList[leaderData[0]],
+                color: colorList[leaderData[0]],
+                fillOpacity: 0.5,
+                weight: 1
+        })
+        .setRadius(getRadius(leaderData[2]))
+        .bindPopup(_.template($("#template-popup").html(), feature.properties)
+        );
+        return marker;
         }
-        var str = str.toLowerCase();
-        return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-    };
-    function getPercentage(value, total) {
-        return ((value/total)*100).toFixed(1);
-    }
-
-    var $width = $(window).width();
-    var center = [initialLat, initialLon];
-    var zoom = 8;
-
-    if ($width <= 767) {
-        center = [initialLat, initialLon];
-        zoom = 7;
-    }
-
-    if ($width <= 479) {
-        zoom = 7;
-    }
-
-    var map = L.map('map', {
-        zoomControl: false,
-        minZoom: 8
-    }).setView(center, zoom);
-    // Base tiles
-    var mapquestLayer = new L.TileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-        attribution: 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>,<a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.',
-        subdomains: ['otile1','otile2','otile3','otile4']
     });
-    map.addLayer(mapquestLayer);
 
-    // Styling tricks
-    var radiusConstant = 0.5;
-    var getRadius = function(value) {
-        return Math.sqrt(value)*radiusConstant;
-    };
+    // Set the default data layer that will load with the map
+    map.addLayer(circleLayer);
 
-    colorList = setColors(listNames);
-    // Fire off to get our GeoJSON
-    $.getJSON(geoJsonUrl, function(response) {
+    // Add a little headline in there too
+    $(".leaflet-control-layers-list").prepend("<h3>Data styles</h3>");
 
-        //get parties and assign colors
-
-        // Pull in the circle layer from GeoJSON
-        var circleLayer = L.geoJson(response, {
-            pointToLayer: function (feature, latlng) {
-                var leaderData = getLeader(feature['properties']);
-                var marker = new L.circleMarker(latlng, {
-                    fillColor: colorList[leaderData[0]],
-                    color: colorList[leaderData[0]],
-                    fillOpacity: 0.5,
-                    weight: 1
-            })
-            .setRadius(getRadius(leaderData[2]))
-            .bindPopup(_.template($("#template-popup").html(), feature.properties)
-            );
-            return marker;
-            }
-        });
-
-        // Set the default data layer that will load with the map
-        map.addLayer(circleLayer);
-
-        // Add a little headline in there too
-        $(".leaflet-control-layers-list").prepend("<h3>Data styles</h3>");
-
-        // Add the zoom control in the upper right
-        new L.Control.Zoom({position: "topright"}).addTo(map);
-    });
+    // Add the zoom control in the upper right
+    new L.Control.Zoom({position: "topright"}).addTo(map);
+});
