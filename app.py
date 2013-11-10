03a190720580fa8553eaaac95cc5512fa86f899b
Franco M. Luque <francolq@gmail.com>
2013-11-10 19:23:41 -0300

    some fixes in web page text.

--- app.py
+++ app.py
@@ -7,19 +7,23 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html',
-        headline = "Resultados Hackaton 2013 Cordoba",
+        headline = u"Resultados Hackatón 2013 Córdoba",
         pubdate = "Nov. 9, 2013",
-        description = """
-<p>Los resultados de las elecciones de diputados de Octubre en las escuelas donde los ciudadanos de Cordoba votaron </p>
-
-<p>Cada circulo representa una escuela y el color representa al partido que obtuvo la mayoria de votos en esa escuela.</p>
+        description = u"""
+<p>Resultados de las elecciones de diputados de Octubre en las escuelas a donde los ciudadanos de Córdoba votaron.</p>
+<p>Cada círculo representa una escuela y el color representa al partido que obtuvo la mayoría de votos en esa escuela.</p>
 """,
-        disclaimer = """Los porcentajes y las cantidades pertenecen a los votos de 90% de las escuelas que estan geolocalizadas, hay un 10% que no esta representado y falta geolocalizar, es por eso que los numeros finales puede diferir de los del escrutinio provisorio""",
-        about = """
-<p>
+        disclaimer = u"""Los resultados totales corresponden sólo a los votos de las escuelas que están geolocalizadas.
+                     Falta geolocalizar aproximadamente un 10% de las escuelas.
+                     Es por eso que los números totales pueden diferir de los del escrutinio oficial.""",
+        about = u"""
+<p>Este mapa fue realizado en el marco del <a href="http://democraciaconcodigos.github.io/">1er Hackatón Sobre Datos Públicos de Córdoba: Democracia con Códigos</a>.
+El código se encuentra disponible en <a href="https://github.com/democraciaconcodigos/election-2013">GitHub</a>.
+</p>
+<!--p>
     This is the test map taken from <a href="http://blogs.lanacion.com.ar/datafest/">Datafest 2013</a>.
     The source is <a href="http://palermo-hollywood.github.io/election-2013/">available on GitHub</a>.
-</p>
+</p-->
 """,
     initial_lat=-31.94284,
     initial_lon=-63.599854
