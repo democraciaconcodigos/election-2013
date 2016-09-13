955f757261acaf4805a4b1f5baf1e77041641dd4
pdelboca <patriciodelboca@gmail.com>
2016-09-13 17:27:17 -0300

    Cambiado el Layer Tile de Leaflet a OpenStreetMaps.

--- static/script.js
+++ static/script.js
@@ -111,10 +111,8 @@ var map = L.map('map', {
     minZoom: 8
 }).setView(center, zoom);
 // Base tiles
-var mapquestLayer = new L.TileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>,<a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.',
-    subdomains: ['otile1','otile2','otile3','otile4']
+var mapquestLayer = new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
 map.addLayer(mapquestLayer);
 
